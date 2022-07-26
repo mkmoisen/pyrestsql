82b00de2c0966118ba63324f2601e25ab53f8081
mmoisen <mmoisen@cisco.com>
2022-07-25 20:55:33 -0700

    swagger ui

--- setup.py
+++ setup.py
@@ -5,5 +5,13 @@ setup(
     version='0.0.1',
     packages=find_packages(include=['pyrestsql', 'pyrestsql.*']),
     package_dir={'pyrestsql': 'pyrestsql'},
-    package_data={'pyrestsql': ['swagger/templates/swagger/*.html']},
+    package_data={
+        'pyrestsql': [
+            'swagger/templates/swagger/*.html',
+            'swagger/static/css/*.css',
+            'swagger/static/img/*.png',
+            'swagger/static/js/*.js',
+            'swagger/static/js/*.map',
+        ]
+    },
 )
