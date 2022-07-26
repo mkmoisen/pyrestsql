a15e1981a22344d4e19fc17f67f3e58d0672a28f
mmoisen <mmoisen@cisco.com>
2022-07-25 20:53:11 -0700

    swagger ui

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyrestsql',
     version='0.0.1',
-    packages=find_packages(include=['pyrestsql', 'pyrestsql.*'])
+    packages=find_packages(include=['pyrestsql', 'pyrestsql.*']),
+    package_dir={'pyrestsql': 'pyrestsql'},
+    package_data={'pyrestsql': ['swagger/templates/swagger/*.html']},
 )
